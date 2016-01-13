8a80e53cdc5e6615e8124da8f1385a88baaf4f08
Daren Schaad and Neil Larion <darenschaad@gmail.com>
2016-01-13 09:31:21 -0800

    Add content and buttons for snake/turtle/insect site

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,12 @@
 $(function(){
   //something here
-
+  $("button").click(function(){
+    if($(this).attr('id')==="snake"){
+      $(".snake").show();
+    } else if ($(this).attr('id')==="insect"){
+      $(".insect").show();
+    } else {
+      $(".turtle").show();
+    }
+  });
 });
